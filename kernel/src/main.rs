80b586062f717b5835f43fdd15d71959e4306516
Nathaniel Nemenzo <nathanielnemenzo2@gmail.com>
2025-12-30 12:38:11 -0700

    limine bootloader

---
+++ kernel/src/main.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+
+use limine::BaseRevision;
+use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker};
+
+#[used]
+#[unsafe(link_section = ".requests")]
+static BASE_REVISION: BaseRevision = BaseRevision::new();
+
+#[used]
+#[unsafe(link_section = ".requests")]
+static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+
+#[used]
+#[unsafe(link_section = ".requests_start_marker")]
+static _START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
+
+#[used]
+#[unsafe(link_section = ".requests_end_marker")]
+static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() {
+    assert!(BASE_REVISION.is_supported());
+
+    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
+        if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
+            for i in 0..100_u64 {
+                // Calculate the pixel offset using the framebuffer information we obtained above.
+                // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
+                let pixel_offset = i * framebuffer.pitch() + i * 4;
+
+                // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
+                unsafe {
+                    framebuffer
+                        .addr()
+                        .add(pixel_offset as usize)
+                        .cast::<u32>()
+                        .write(0xFFFFFFFF)
+                };
+            }
+        }
+    }
+
+    hcf();
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    hcf();
+}
+
+fn hcf() -> ! {
+    loop {
+        unsafe {
+            core::arch::asm!("hlt");
+        }
+    }
+}
